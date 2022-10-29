50575324ca15a75394ac5da39db2505c2ebf0112
Aiwii <53021785+Aiwii@users.noreply.github.com>
2022-10-29 13:44:50 -0600

    First commit barely any code yo

---
+++ gamefiles/PlayerStats.gd
@@ -0,0 +1,28 @@
+extends Node
+
+export (String) var pname
+export (int,0,10) var speed #how much time passes between houses
+export (int,0,10) var charm #how much candy is dropped
+export (int,0,10) var skill #likihood of door being answered
+export (int) var apoints
+
+export (int,0,999) var kingsize
+export (int,0,999) var cans
+export (int,0,999) var treatbags
+export (int,0,999) var snacksize
+export (int,0,999) var chips
+export (int,0,999) var candies
+export (int,0,999) var water
+export (int,0,999) var veggies
+export (int,0,999) var fruit
+ 
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
